7006ab6f2ccb02ebd8a9ef3eb250ccd60f790af0
Dania Amador <daniajacquelinp@gmail.com>
2023-07-18 14:08:33 -0700

    Test and Code context for scoops subtotal

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
-import logo from "./logo.svg";
-import "./App.css";
 import React from "react";
-import SummaryForm from "./pages/summary/SummaryForm";
+import { Container } from "react-bootstrap";
+import OrderEntry from "./pages/entry/OrderEntry";
+import { OrderDetailsProvider } from "./contexts/OrderDetails";
 
 function App() {
   return (
-    <div className="App">
-      <SummaryForm></SummaryForm>
-    </div>
+    <Container>
+      <OrderDetailsProvider>
+        {/* Summary page and entry page need rprovider*/}
+        <OrderEntry />
+      </OrderDetailsProvider>
+      {/* confirmation page does not need provider*/}
+    </Container>
   );
 }
 
